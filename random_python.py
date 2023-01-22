cbcd015d5ad276da188ec3b942bbd8a030cf3945
realVinayak <realvinayak123@gmail.com>
2023-01-22 16:39:56 -0600

    Add loading and plotting from file

--- random_python.py
+++ random_python.py
@@ -43,6 +43,28 @@ class Random:
     def rand(self):
         return 5.42101086242752217E-20 * self.int64()
 
+def show_plot(random_number_list):
+    n, bins, patches = plt.hist(random_number_list, 50, density=True, facecolor='g', alpha=0.75)
+
+    # plot formating options
+    plt.xlabel('x')
+    plt.ylabel('Probability')
+    plt.title('Uniform random number')
+    plt.grid(True)
+
+    # show figure (program only ends once closed
+    plt.show()
+
+def load_random_num_from_file(file_name):
+    #Assumes random_num are stored as string with '\n' as delimiter
+    random_num_file = open(file_name, 'r')
+    random_nums_str = random_num_file.read()
+    random_num_file.close()
+
+    random_nums_list = [float(random_num_str) for random_num_str in random_nums_str.split('\n')]
+    show_plot(random_nums_list)
+
+
 # main function for this Python code
 if __name__ == "__main__":
     # if the user includes the flag -h or --help print the options
@@ -66,7 +88,7 @@ if __name__ == "__main__":
     random = Random(seed)
 
     # create some random data
-    N = 100
+    N = 10000
 
     # an array of random numbers from numpy
     x = np.random.rand(N)
@@ -79,14 +101,8 @@ if __name__ == "__main__":
     random_file = open("random_number.txt", 'w')
     random_file.write('\n'.join([str(random_num) for random_num in myx]))
     random_file.close()    
+
+    load_random_num_from_file("random_number.txt")
     # create histogram of our data
-    n, bins, patches = plt.hist(myx, 50, density=True, facecolor='g', alpha=0.75)
-
-    # plot formating options
-    plt.xlabel('x')
-    plt.ylabel('Probability')
-    plt.title('Uniform random number')
-    plt.grid(True)
-
-    # show figure (program only ends once closed
-    plt.show()
+    #Uncomment the following line to directly plot the random nums
+    #show_plot(myx)
